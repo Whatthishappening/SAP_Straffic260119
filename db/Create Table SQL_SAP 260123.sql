4ccbda03bb67b1a985b22fc765e3a9a64175243a
BaeYoungWhan <uee8351773@naver.com>
2026-02-02 19:48:20 +0900

    260202 Spring Boot 및 Vue 생성 완료. 단위테스트 진행 package.json vite : 7.2.4 > 7.3.1 변경 이유 모름.

--- db/Create Table SQL_SAP 260123.sql
+++ db/Create Table SQL_SAP 260123.sql
@@ -60,6 +60,11 @@ select * from storage.local_amenities;
 
 select * from storage.locker_usage order by locker_usage.created_at desc
 
+select * from storage.user;
+--update storage.user set auth = 1 where user_name = '배영환';
+
+insert 
+
 -- 유저 테이블 생성
 -- CREATE TABLE storage.user(
 -- 	user_id				VARCHAR(20)		primary key,
